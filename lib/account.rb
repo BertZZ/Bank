05031686278245c4286c9ec28ab04e3fc6b7adb6
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-06-12 11:30:20 +0100

    Adds methods to account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,16 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
+  end
+
+  def deposit(number)
+    @balance = @balance + number
+  end
+
+  def withdraw(number)
+    @balance = @balance - number
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,24 @@ describe Account do
   it 'has a starting balance of 0' do
     expect(subject.balance).to equal(0)
   end
+
+  it 'starts with an empty array of transactions' do
+    expect(subject.transactions).to be_a(Array)
+    expect(subject.transactions).to be_empty
+  end
+
+  describe '#deposit' do
+    it 'Adds money to the balance' do
+      account.deposit(300)
+      expect(subject.balance).to equal(300)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'Removes money from the account' do
+      account.deposit(1000)
+      account.withdraw(300)
+      expect(subject.balance).to equal(700)
+    end
+  end
 end
